01a6616b710fe205f8c25987138351f0f6675014
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-17 10:35:38 +0000

    String and character input from the user

---
+++ ChangeString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Write a string: ");
+            string words = Console.ReadLine();
+            
+            Console.WriteLine("Write a character: ");
+            string character = Console.ReadLine();
+            
+                
+        }
+    }
+}
