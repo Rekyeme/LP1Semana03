491ba181649c90754821a516ddcbcb10a71b5012
Rekyeme <samuelcarvalho.9966@gmail.com>
2023-03-17 11:09:09 +0000

    First commit and user input

---
+++ ArraySums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the number of horizontal lines for the array: "); 
+            int x = Console.Read();
+
+            Console.WriteLine("Write the number of vertical lines for the array: ");
+            int y = Console.Read();
+
+            float[,] matrix = new float[x, y];
+        }
+    }
+}
